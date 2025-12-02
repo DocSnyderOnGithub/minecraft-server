a296f519fa00457f689135ac1f49ae38ad5d3ab5
Doc Snyder <github@abi97nidda.de>
2025-12-02 19:55:43 +0100

    Update update.sh
    added worldguard, worldedit and Portals

--- server/update.sh
+++ server/update.sh
@@ -37,8 +37,17 @@ for module in admin bedtime core enchantments permissions portals regions trifle
 	download_latest_github_release "oddlama/vane" "vane-$module-{VERSION}.jar" "plugins/vane-$module.jar"
 done
 
-download_file "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar" plugins/ProtocolLib.jar
-download_latest_github_release "BlueMap-Minecraft/BlueMap" "BlueMap-{VERSION}-spigot.jar" plugins/bluemap.jar
+#download_file "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar" plugins/ProtocolLib.jar
+# url changed...
+download_file "https://github.com/dmulloy2/ProtocolLib/releases/download/5.4.0/ProtocolLib.jar" plugins/ProtocolLib.jar
+# download_latest_github_release "BlueMap-Minecraft/BlueMap" "BlueMap-{VERSION}-spigot.jar" plugins/bluemap.jar
+download_file "https://hangarcdn.papermc.io/plugins/Blue/BlueMap/versions/5.13/PAPER/bluemap-5.13-paper.jar" plugins/bluemap.jar
+
+download_from_modrinth "advanced-portals" "Paper" plugins/Advanced-Portals.jar {TAG}
+download_from_modrinth "worldedit" "Paper" plugins/Advanced-Portals.jar {TAG}
+download_file "https://dev.bukkit.org/projects/worldguard/files/latest" plugins/worldguard.jar
+# https://cdn.modrinth.com/data/1u6JkXh5/versions/3ISh7ADm/worldedit-bukkit-7.3.17.jar
+
 download_from_json_feed \
   "https://ci.lucko.me/job/spark/lastSuccessfulBuild/api/json" \
   '.artifacts[]
