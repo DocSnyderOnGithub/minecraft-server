f358cbfeafd03be7d7067c1085e4a954ceccaab2
Patrick Schneider <patrick.schneider@codecentric.de>
2025-12-03 01:05:31 +0100

    testing downloads

--- server/update.sh
+++ server/update.sh
@@ -33,19 +33,24 @@ mkdir -p plugins/optional \
 # - download_from_hangar (project) (platform) (output file)
 # - download_from_modrinth (mod ID/name) (platform) (output file) [minecraft version]
 
-substatus "Downloading plugins"
+substatus "Downloading vane plugins"
 for module in admin bedtime core enchantments permissions portals regions trifles; do
 	download_latest_github_release "oddlama/vane" "vane-$module-{VERSION}.jar" "plugins/vane-$module.jar"
 done
 
+substatus "Downloading ProtocolLib from github"
 #download_file "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar" plugins/ProtocolLib.jar
 # url changed...
 download_file "https://github.com/dmulloy2/ProtocolLib/releases/download/5.4.0/ProtocolLib.jar" plugins/ProtocolLib.jar
 # download_latest_github_release "BlueMap-Minecraft/BlueMap" "BlueMap-{VERSION}-spigot.jar" plugins/bluemap.jar
+substatus "Downloading Bluemap from hangar"
 download_file "https://hangarcdn.papermc.io/plugins/Blue/BlueMap/versions/5.13/PAPER/bluemap-5.13-paper.jar" plugins/bluemap.jar
 
-download_from_modrinth "advanced-portals" "Paper" plugins/Advanced-Portals.jar ${PREFERRED_PAPER_VERSION}
-download_from_modrinth "worldedit" "Paper" plugins/Advanced-Portals.jar ${PREFERRED_PAPER_VERSION}
+substatus "Downloading Advanced Portals from modrinth"
+download_from_modrinth "advanced-portals" "paper" plugins/Advanced-Portals.jar ${PREFERRED_PAPER_VERSION}
+substatus "Downloading Worldedit from modrinth"
+download_from_modrinth "worldedit" "paper" plugins/Advanced-Portals.jar ${PREFERRED_PAPER_VERSION}
+substatus "Downloading Worldguard from bukkit.org"
 download_file "https://dev.bukkit.org/projects/worldguard/files/latest" plugins/worldguard.jar
 # https://cdn.modrinth.com/data/1u6JkXh5/versions/3ISh7ADm/worldedit-bukkit-7.3.17.jar
 
