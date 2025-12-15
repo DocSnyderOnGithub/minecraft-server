bda56933da37f3b590480662996397a9b27d210e
Doc Snyder <github@abi97nidda.de>
2025-12-15 02:57:35 +0100

    Update update.sh to get update for 1.21.11

--- server/update.sh
+++ server/update.sh
@@ -9,7 +9,8 @@ become_minecaft "./update.sh"
 
 ################################################################
 # Download paper and prepare plugins
-PREFERRED_PAPER_VERSION="1.21.10"
+PREFERRED_PAPER_VERSION="1.21.11"
+FALLBACK_PAPER_VERSION="1.21.10"
 
 download_paper paper.jar ${PREFERRED_PAPER_VERSION}
 
@@ -42,16 +43,25 @@ substatus "Downloading ProtocolLib from github"
 #download_file "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar" plugins/ProtocolLib.jar
 # url changed...
 download_file "https://github.com/dmulloy2/ProtocolLib/releases/download/5.4.0/ProtocolLib.jar" plugins/ProtocolLib.jar
+
+substatus "Downloading Bluemap from github/hangar"
 # download_latest_github_release "BlueMap-Minecraft/BlueMap" "BlueMap-{VERSION}-spigot.jar" plugins/bluemap.jar
-substatus "Downloading Bluemap from hangar"
-download_file "https://hangarcdn.papermc.io/plugins/Blue/BlueMap/versions/5.13/PAPER/bluemap-5.13-paper.jar" plugins/bluemap.jar
+# download_file "https://hangarcdn.papermc.io/plugins/Blue/BlueMap/versions/5.15/PAPER/bluemap-5.15-paper.jar" plugins/bluemap.jar
+download_from_hangar BlueMap PAPER plugins/bluemap.jar
 
 substatus "Downloading WorldEdit from modrinth"
 download_from_modrinth "worldedit" "paper" plugins/worldedit.jar ${PREFERRED_PAPER_VERSION}
+
 substatus "Downloading WorldGuard from modrinth"
-download_from_modrinth "worldguard" "paper" plugins/worldguard.jar ${PREFERRED_PAPER_VERSION}
-#download_file "https://dev.bukkit.org/projects/worldguard/files/latest" plugins/worldguard.jar
+# download_from_modrinth "worldguard" "paper" plugins/worldguard.jar ${PREFERRED_PAPER_VERSION}
+download_from_modrinth "worldguard" "paper" plugins/worldguard.jar ${FALLBACK_PAPER_VERSION}
+# download_file "https://dev.bukkit.org/projects/worldguard/files/latest" plugins/worldguard.jar  ## !captcha!
 # https://cdn.modrinth.com/data/1u6JkXh5/versions/3ISh7ADm/worldedit-bukkit-7.3.17.jar
+
+substatus "Downloading WorldGuard plugin for BlueMap from github"
+download_latest_github_release "Mark-225/BlueBridge" "BlueBridgeCore-{VERSION}.jar" "plugins/BlueBridgeCore.jar"
+download_latest_github_release "Mark-225/BlueBridge" "BlueBridgeWB-{VERSION}.jar" "plugins/BlueBridgeWB.jar"
+download_latest_github_release "Mark-225/BlueBridge" "BlueBridgeWG-{VERSION}.jar" "plugins/BlueBridgeWG.jar"
 
 substatus "Downloading Advanced Portals from modrinth"
 download_from_modrinth "advanced-portals" "paper" plugins/Advanced-Portals.jar
@@ -59,7 +69,13 @@ download_from_modrinth "advanced-portals" "paper" plugins/Advanced-Portals.jar
 substatus "Downloading teawaystones from modrinth"
 download_from_modrinth "teawaystones" "paper" plugins/Waystones.jar
 
+substatus "Downloading Versioning support from hangar"
+download_from_hangar ViaVersion PAPER plugins/ViaVersion.jar
+download_from_hangar ViaBackwards PAPER plugins/ViaBackwards.jar
+# download_from_hangar Geyser PAPER plugins/Geyser.jar
 
+
+substatus "Downloading spark server profiler from json feed"
 download_from_json_feed \
   "https://ci.lucko.me/job/spark/lastSuccessfulBuild/api/json" \
   '.artifacts[]
